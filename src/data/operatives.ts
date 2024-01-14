ac587e39879c453531803b6f3a33d7f3ab515d5c
JonR205 <gohan205@gmail.com>
2024-01-14 17:18:55 -0600

    Added Operatives and included in dataslate

---
+++ src/data/operatives.ts
@@ -0,0 +1,13 @@
+type Specialism = 'Combat' | 'Staunch' | 'Marksman' | 'Scout'
+type Rank = 'Adept' | 'Veteran' | 'Ace' | 'Grizzled' | 'Revered'
+export interface Operatives {
+  name: string
+  type: string
+  specialisms: Specialism[]
+  xp: number
+  rank: Rank
+  battleHonours: []
+  battleScars: []
+  Resttally: number
+  notes: string
+}
