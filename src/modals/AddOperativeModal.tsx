dceb3444cdbcf321271c8c8d273c12472e00326d
JonR205 <gohan205@gmail.com>
2024-01-30 15:00:09 -0600

    Started using useref

--- src/modals/AddOperativeModal.tsx
+++ src/modals/AddOperativeModal.tsx
@@ -4,18 +4,19 @@ import useDataslateStore from '../stores/dataslateStore.ts'
 import { Dataslate, updateDataslate } from '../data/dataslate.ts'
 import { useForm } from 'react-hook-form'
 import { Operatives } from '../component/Operatives.tsx'
+import { useRef } from 'react'
 
-type FormData = {
-  name?: string
-  type: string
-  specialisms: []
-  xp: 0
-  rank: 'Adept'
-  battleHonours: []
-  battleScars: []
-  restTally: number
-  notes?: string
-}
+// type FormData = {
+//   name?: string
+//   type: string
+//   specialisms: []
+//   xp: 0
+//   rank: 'Adept'
+//   battleHonours: []
+//   battleScars: []
+//   restTally: number
+//   notes?: string
+// }
 
 interface Props {
   showOperativemodal: boolean
@@ -28,22 +29,24 @@ function AddOperativeModal(props: Props) {
 
   const isActive = showOperativemodal ? 'is-active' : ''
 
-  const session = useContext(sessionContext)
-  const updateDataslate = useDataslateStore((state) => state.addOperative)
+  const saveBaseInfo = useDataslateStore((state) => state.saveBaseInfo)
 
   const factionOperatives = useDataslateStore(
     (state) => state.selectedDataslate?.faction.operatives,
   )
 
-  const {
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({})
+  const selectedOperatives = useDataslateStore(
+    (state) => state.selectedDataslate?.operatives,
+  )
+  const operativeNameRef = useRef<HTMLInputElement>(null)
+  const operativeTypeRef = useRef<HTMLSelectElement>(null)
 
-  const onSubmit = (data: FormData) => {
-    factionOperatives?.push(data)
+  const onSave = () => {
+    const name = operativeNameRef.current?.value ?? ''
+    const type = operativeTypeRef.current?.value ?? ''
 
-    onClose()
+    saveBaseInfo(name, type)
+    props.onClose()
   }
 
   return (
@@ -57,28 +60,46 @@ function AddOperativeModal(props: Props) {
             <p className="modal-card-title"> Operatives</p>
             <button className="delete" onClick={onClose}></button>
           </header>
-          <form>
-            <div className="field">
-              <label className="lable"> Chose Operative </label>
-              <div className="control" />
-              <div className="select">
-                <select name="operative" id="op" defaultValue="">
-                  <option value=""> Select From Dropdown</option>
-                  {factionOperatives?.map((operative, index) => (
-                    <option key={index} value={operative.type}>
-                      {operative.type}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <button className="button is-primary" onClick={handleSubmit}>
-                Submit
-              </button>
-              <button className="button" onClick={onClose}>
-                Cancel
-              </button>
+          <div className="field">
+            <label className="lable"> Chose Operative Type</label>
+            <div className="control" />
+            <div className="select">
+              <select
+                name="operative"
+                id="op"
+                defaultValue=""
+                className="seletct"
+                ref={operativeTypeRef}>
+                <option value=""> Select From Dropdown</option>
+                {factionOperatives?.map((operative, index) => (
+                  <option key={index} value={operative.type}>
+                    {operative.type}
+                  </option>
+                ))}
+              </select>
             </div>
-          </form>
+            <button className="button is-primary" onClick={onSave}>
+              Submit
+            </button>
+            <button className="button" onClick={onClose}>
+              Cancel
+            </button>
+            <div className="container">
+              <section className="section">
+                <div className="columns is-multiline">
+                  {selectedOperatives?.map(({ type }, index) => (
+                    <p
+                      className={
+                        'is-size-6 has-text-weight-bold has-text-grey-lighter'
+                      }
+                      key={index}>
+                      {type}
+                    </p>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </div>
         </div>
       </div>
     </>
